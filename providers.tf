ae9b6ed54ebc56a61bd56c5192bdac505f63933a
RaaviSivajiTech <155118588+RaaviSivajiTech@users.noreply.github.com>
2024-04-11 13:56:10 -0400

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -22,11 +22,7 @@ terraform {
 }
 
 provider "azurerm" {
-  features {}
-  client_id       = $AZURE_CLIENT_ID
-  client_secret   = $AZURE_CLIENT_SECRET
-  tenant_id       = $AZURE_TENANT_ID
-  subscription_id = $AZURE_SUBSCRIPTION_ID 
+  features {}  
 }
 
 provider "aws" {
