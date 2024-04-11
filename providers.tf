64bc832cb9d3428b85990d64e5b6970119110f5e
RaaviSivajiTech <155118588+RaaviSivajiTech@users.noreply.github.com>
2024-04-11 14:05:45 -0400

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -22,7 +22,11 @@ terraform {
 }
 
 provider "azurerm" {
-  features {}  
+  features {}
+  client_id       = "ce482ba4-b218-42d0-a409-9b322287c316"
+  client_secret   = ".L08Q~nmHJqUKQFEbWYQxLsoynevLSe1nd7s4a0d"
+  tenant_id       = "1670acf3-2fff-4a6f-8b88-de7e6cf2a5aa"
+  subscription_id = "e7948db7-f07a-4d41-8c51-52b3f8e44f9f"
 }
 
 provider "aws" {
